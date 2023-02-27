61402509681681229b07100be19b98a8cc7e2091
mmlvgx <109544945+mmlvgx@users.noreply.github.com>
2023-02-27 20:10:21 +0300

    Delete lexer.pyi

--- lexer.pyi
+++
@@ -1,9 +0,0 @@
-from tokenizer import Token
-
-
-class Lexer:
-    def __init__(self, lineList: list[str], tokenList: list[Token]) -> None:
-        self.lineList = lineList
-        self.tokenList = tokenList
-
-    def analyze(self, filename: str) -> list[Token]: ...
